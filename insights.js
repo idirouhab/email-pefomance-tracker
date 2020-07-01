2cd8d13f37d5f1b91915edd838d603476d8eaefc
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2020-07-01 19:00:36 +0200

    feat: add support for proxy

--- insights.js
+++ insights.js
@@ -1,22 +1,24 @@
-const request = require('request');
+const axios = require("axios");
 
 module.exports.sendToInsights = (payload, newRelic) => {
-    if (global.debug) console.log('Sending data to New Relic');
-    const options = {
-        method: 'POST',
-        url: `https://insights-collector.newrelic.com/v1/accounts/${newRelic.accountId}/events`,
-        headers: {
-            'content-type': 'application/json',
-            'x-insert-key': newRelic.insertKey
-        },
-        json: true,
-    };
-
-    payload['eventType'] = 'Email';
-    options['body'] = [payload];
-
-    request(options, (error, response, body) => {
-        if (error) throw new Error(error);
-        if (global.debug) console.log(body);
+    const proxy = global.proxy ? global.proxy : false;
+    const axiosClient = axios.create({
+        timeout: 3000,
+        proxy: proxy
     });
+
+    if (global.debug) console.log('Sending data to New Relic');
+    payload['eventType'] = 'Email';
+
+    return axiosClient
+        .post(
+            `https://insights-collector.newrelic.com/v1/accounts/${newRelic.accountId}/events`,
+            payload,
+            {
+                headers: {
+                    'content-type': 'application/json',
+                    'x-insert-key': newRelic.insertKey
+                }
+            }
+        );
 };
